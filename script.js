663aa388301f300533a04d20f3e69580d2e7b550
avazbek489 <obidjonovavazbek787@gmail.com>
2024-09-29 10:13:17 +0500

    beginne

---
+++ script.js
@@ -0,0 +1,179 @@
+// // 1-SAVOL JAVOBI IF
+
+// let num = +prompt("Sonni kiriting");
+// if (num > 0) {
+//   num = nu = m + 1;
+// } else {
+//   num = num + 0;
+// }
+// console.log(num);
+
+// // 2-SAVOL JAVOBI
+
+// let num = +prompt("Sonni kiriting");
+// if (num > 0) {
+//   num = num + 1;
+// } else {
+//   num = num - 2;
+// }
+// console.log(num);
+
+// // 3-SAVOL JAVOBI
+
+// let num = +prompt("Sonni kiriting");
+// if (num > 0) {
+//   num = num + 1;
+// } else if (num == 0) {
+//   num = num + 10;
+// } else {
+//   num = num - 2;
+// }
+// console.log(num);
+
+// // 4-SAVOL JAVOBI
+
+//  let num1 = +prompt("1-sonni kiriting");
+//  let num2 = +prompt("2-sonni kiriting");
+//  let num3 = +prompt("3-sonni kiriting");
+
+// let count = 0;
+
+// if (num1 > 0) {
+//   count = count + 1;
+// }
+// if (num2 > 0) {
+//   count = count + 1;
+// }
+// if (num3 > 0) {
+//   count = count + 1;
+// }
+
+// console.log(count);
+
+// // 5-SAVOL JAVOBI
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let count = 0;
+// let sum = 0;
+
+// if (num1 > 0) {
+//   count = count + 1;
+// } else {
+//   sum = sum + 1;
+// }
+// if (num2 > 0) {
+//   count = count + 1;
+// } else {
+//   sum = sum + 1;
+// }
+// if (num3 > 0) {
+//   count = count + 1;
+// } else {
+//   sum = sum + 1;
+// }
+// console.log("Musbatlar soni:", count);
+// console.log("Manfiylar soni:", sum);
+
+// // 6-SAVOL JAVOBI
+//  let num1 = +prompt("1-Sonni kiriting")
+//  let num2 = +prompt("2-Sonni kiriting")
+//  let sum;
+
+//  if(num1 > num2){
+//     sum = num1
+//  } else{
+//     sum = num2
+//  }
+
+//  console.log("Eng katta son:",sum);
+
+// // 7-SAVOL JAVOBI
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if (num1 < num2) {
+//   console.log("Eng kichik sonning tartib raqami: 1");
+// } else if (num1 > num2) {
+//   console.log("Eng kichik sonning tartib raqami: 2");
+// } else {
+//   console.log("2ta son ozaro teng.");
+// }
+
+// // 8-SAVOL JAVOBI
+// let num1 = +prompt("1-Sonni kiriting");
+// let num2 = +prompt("2-Sonni kiriting");
+// if (num1 > num2) {
+//   alert("Eng kattasi: " + num1 + "\nEng kichigi: " + num2);
+// } else if (num1 < num2) {
+//   alert("Eng kattasi: " + num2 + "\nEng kichigi: " + num1);
+// } else {
+//   alert("Ikkala son teng: " + num1);
+// }
+
+// // 9-SAVOL JAVOBI
+// let A = +prompt("A sonni kiriting:");
+// let B = +prompt("B sonni kiriting:");
+
+// if (A >= B) {
+//   let res = A;
+//   A = B;
+//   B = res;
+// }
+
+// alert("A: " + A + "\nB: " + B);
+
+// // 10-SAVOL JAVOBI
+// let A = +prompt("A sonni kiriting:");
+// let B = +prompt("B sonni kiriting:");
+// let res;
+
+// if(A != B){
+//     res = A + B
+// }
+
+// // 11-SAVOL JAVOBI
+// let A = +prompt("A sonini kiriting:");
+// let B = +prompt("B sonini kiriting:");
+
+// if (A != B) {
+//   if (A > B) {
+//     B = A;
+//   } else {
+//     A = B;
+//   }
+// } else {
+//   A = 0;
+//   B = 0;
+// }
+
+// alert("A: " + A + "\nB: " + B);
+
+// // 12-SAVOL JAVOBI
+let num1 = +prompt("1-sonni kiriting:");
+let num2 = +prompt("2-sonni kiriting:");
+let num3 = +prompt("3-sonni kiriting:");
+let res = num1;
+if (num2 < res) {
+  res = num2;
+}
+if (num3 < res) {
+  res = num3;
+}
+alert("Eng kichik son:" + res);
+
+// // 13-SAVOL JAVOBI
+// // 14-SAVOL JAVOBI
+// // 15-SAVOL JAVOBI
+// // 16-SAVOL JAVOBI
+// // 17-SAVOL JAVOBI
+// // 18-SAVOL JAVOBI
+// // 19-SAVOL JAVOBI
+// // 20-SAVOL JAVOBI
+// // 21-SAVOL JAVOBI
+// // 22-SAVOL JAVOBI
+// // 23-SAVOL JAVOBI
+// // 24-SAVOL JAVOBI
+// // 25-SAVOL JAVOBI
+// // 26-SAVOL JAVOBI
